3eea22997e9d459b19bc6e5fd9e4f0d19148fdd1
kento-ogata <k.ogata1013@gmail.com>
2021-11-26 09:21:54 +0900

    feat: add parsers

--- nvim/plugin/treesitter.lua
+++ nvim/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require 'nvim-treesitter.configs'.setup {
-	ensure_installed = { 'vim','toml' },
+	ensure_installed = { 'vim','toml','c','cpp','c_sharp' },
 	indent = { enable = true },
 	incremental_selection = { enable = true },
 	highlight = { enable = true }
