1c4793ef39e6e8a817ee9969f06c176be7289c92
kento-ogata <k.ogata1013@gmail.com>
2022-05-13 18:12:33 +0900

    fix(treesitter): update injection setting of dein.toml

--- nvim/after/queries/toml/injections.scm
+++ nvim/after/queries/toml/injections.scm
@@ -1,8 +1,13 @@
 ((pair
   (bare_key) @_key
   (string) @vim)
- (#vim-match? @_key "^hook_\w*")
+ (#vim-match? @_key "^hook_\w*")(#match? @vim "^\([']{3}.*[']{3}\)$")
  (#offset! @vim 0 3 0 -3))
+((pair
+  (bare_key) @_key
+  (string) @vim)
+ (#vim-match? @_key "^hook_\w*")(#match? @vim "^\([']{1}.*[']{1}\)$")
+ (#offset! @vim 0 1 0 -1))
 ((table
   (bare_key) @_key
   (pair
